5b6d258923b6e0b2dd2d059943e599af26ba7b38
Nghi Nguyen <ndnghi.2011@gmail.com>
2023-03-22 10:48:40 +0700

    product-detail page

--- public/js/main.js
+++ public/js/main.js
@@ -134,7 +134,7 @@
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
 	if (zoomMainProduct) {
-		$('#product-main-img .product-preview').zoom();
+		$('#product-main-img .product-preview').zoom({ magnify: 0.7 });
 	}
 
 	/////////////////////////////////////////
